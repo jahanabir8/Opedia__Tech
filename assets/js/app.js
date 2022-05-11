41dae39c9424b93db9599e2b3ecdd167c72daa5a
jahanabir8 <abir.opediatech@gmail.com>
2022-05-11 19:14:30 +0600

    secondary upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,6 @@
   $(".common").click(function(){
     $(".menu").toggleClass("open");
   })
-//   $(".cross__menu").click(function(){
-//     $(".menu").removeClass("open animate__fadeInTopRight");
-//   })
 
 
 
@@ -15,11 +12,56 @@ $('.fadeOut').owlCarousel({
   loop: true,
   margin: 10,
   smartSpeed: 1000,
+  dots : true,
+  dotsData :true,
   // autoplay: true,
   autoplayTimeout : 2000,
-  URLhashListener:true,
-        autoplayHoverPause:true,
-        startPosition: 'URLHash'
-});
   
+});
+
+// hannan miah
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
+
+var swiper = new Swiper(".serviceSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // speed: 1000,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  breakpoints: {
+    1199: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+    },
+    575: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+},
+
+});
  
